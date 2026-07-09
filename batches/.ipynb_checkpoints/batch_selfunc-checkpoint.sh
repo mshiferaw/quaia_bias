d9ff818c69d31f1b464b7d78bbb5baa028fe1565
Mahlet Demiss Shiferaw <mahlet@sh03-ln08.stanford.edu>
2026-07-08 20:31:05 -0700

    update erfc HOD pipeline for wtheta

--- batches/.ipynb_checkpoints/batch_selfunc-checkpoint.sh
+++ batches/.ipynb_checkpoints/batch_selfunc-checkpoint.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-#SBATCH --partition=kipac,hns,normal
-#SBATCH --job-name=selfunc_zminzmax
+#SBATCH --partition=kipac
+#SBATCH --job-name=selfunc_zodi
 #SBATCH --output=logs/%x.out
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=48
 #SBATCH --mem=360GB
-#SBATCH --time=48:00:00
+#SBATCH --time=72:00:00
 
 #conda init 
 conda activate quaia-env
+source /home/users/mahlet/miniconda3/etc/profile.d/conda.sh
 cd /oak/stanford/orgs/kipac/users/mahlet/quaia_bias/scripts
 
 # for zmin in '0.0' '1.0' '2.0' '3.0'
@@ -51,5 +52,15 @@ cd /oak/stanford/orgs/kipac/users/mahlet/quaia_bias/scripts
 #     done
 # done
 
-python selection_function_map.py ../data/quaia_G20.0_zmin3.0zmax4.6.fits ../data/maps/selection_function_NSIDE64_G20.0_zmin3.0zmax4.6.fits -p ../data/quaia_G20.0.fits
-python generate_random.py 20.0 25 _zmin3.0zmax4.6
+# python selection_function_map.py ../data/quaia_G20.0_zmin3.0zmax4.6.fits ../data/maps/selection_function_NSIDE64_G20.0_zmin3.0zmax4.6.fits -p ../data/quaia_G20.0.fits
+# python generate_random.py 20.0 25 _zmin3.0zmax4.6
+
+# python selection_function_map.py ../data/quaia_G20.5_zmin3.0zmax4.6_Lmin-26.0Lmax-25.0.fits ../data/maps/selection_function_NSIDE64_G20.5_zmin3.0zmax4.6_Lmin-26.0Lmax-25.0.fits -p ../data/quaia_G20.5.fits
+# python generate_random.py 20.5 25 _zmin3.0zmax4.6_Lmin-26.0Lmax-25.0
+
+# python selection_function_map.py ../data/quaia_G20.0_zmin0.8zmax2.1.fits ../data/maps/selection_function_NSIDE64_G20.0_zmin0.8zmax2.1.fits -p ../data/quaia_G20.0.fits -m dust stars m10 mcs unwise unwisescan mcsunwise zodi1.25 zodi3.4 zodi4.6
+# python selection_function_map.py ../data/quaia_G20.0_zmin0.8zmax2.1.fits ../data/maps/selection_function_NSIDE64_G20.0_zmin0.8zmax2.1_nozodi.fits -p ../data/quaia_G20.0.fits
+G=20.5
+fname="_zsplit4bin2_Lmaxsplit4bin3"
+python selection_function_map.py ../data/quaia_G${G}${fname}.fits ../data/maps/selection_function_NSIDE64_G${G}${fname}.fits -p ../data/quaia_G${G}.fits -m dust stars m10 mcs unwise unwisescan mcsunwise zodi1.25 zodi3.4 zodi4.6 --overwrite_ypred
+python generate_random.py ${G} 25 ${fname} 
